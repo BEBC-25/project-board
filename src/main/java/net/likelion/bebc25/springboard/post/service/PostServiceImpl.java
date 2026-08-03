45974e362e21054db655fd00e53f6fca9ba0e3ac
kilyong jeong <uzoolove@gmail.com>
2026-08-03 11:41:15 +0900

    파일 업로드 기능 추가 및 주석 추가

--- src/main/java/net/likelion/bebc25/springboard/post/service/PostServiceImpl.java
+++ src/main/java/net/likelion/bebc25/springboard/post/service/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package net.likelion.bebc25.springboard.post.service;
 
+import java.util.List;
 import net.likelion.bebc25.springboard.post.dto.PageDto;
 import net.likelion.bebc25.springboard.post.dto.PostDto;
 import net.likelion.bebc25.springboard.post.repository.PostRepository;
@@ -7,58 +8,85 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
+/**
+ * PostService 인터페이스의 비즈니스 로직 구현 클래스.
+ */
 @Service
 @Transactional(readOnly = true)
 public class PostServiceImpl implements PostService {
 
-    private final PostRepository postRepository;
+  private final PostRepository postRepository;
 
-    public PostServiceImpl(@Qualifier("jdbcTemplatePostRepository") PostRepository postRepository){
-        this.postRepository = postRepository;
-    }
+  /**
+   * PostServiceImpl 생성자.
+   *
+   * @param postRepository 스프링 빈으로 등록된 PostRepository 구현체
+   */
+  public PostServiceImpl(@Qualifier("jdbcTemplatePostRepository") PostRepository postRepository){
+    this.postRepository = postRepository;
+  }
 
-    @Override
-    public List<PostDto> getPosts() {
-        return postRepository.findAll();
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<PostDto> getPosts() {
+    return postRepository.findAll();
+  }
 
-    @Override
-    public List<PostDto> searchPosts(String type, String keyword) {
-        return postRepository.search(type, keyword);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<PostDto> searchPosts(String type, String keyword) {
+    return postRepository.search(type, keyword);
+  }
 
-    @Override
-    public PageDto<PostDto> searchPosts(String type, String keyword, int page, int size) {
-        int validPage = page < 1 ? 1 : page;
-        int validSize = size < 1 ? 10 : size;
-        int offset = (validPage - 1) * validSize;
-        int totalElements = postRepository.count(type, keyword);
-        List<PostDto> posts = postRepository.search(type, keyword, offset, validSize);
-        return new PageDto<>(posts, validPage, validSize, totalElements, 5);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public PageDto<PostDto> searchPosts(String type, String keyword, int page, int size) {
+    int validPage = page < 1 ? 1 : page;
+    int validSize = size < 1 ? 10 : size;
+    int offset = (validPage - 1) * validSize;
+    int totalElements = postRepository.count(type, keyword);
+    List<PostDto> posts = postRepository.search(type, keyword, offset, validSize);
+    return new PageDto<>(posts, validPage, validSize, totalElements, 5);
+  }
 
-    @Override
-    public PostDto getPost(int id) {
-        return postRepository.findById(id);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public PostDto getPost(int id) {
+    return postRepository.findById(id);
+  }
 
-    @Override
-    @Transactional
-    public void writePost(PostDto post) {
-        postRepository.save(post);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  @Transactional
+  public void writePost(PostDto post) {
+    postRepository.save(post);
+  }
 
-    @Override
-    @Transactional
-    public void editPost(PostDto post) {
-        postRepository.update(post);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  @Transactional
+  public void editPost(PostDto post) {
+    postRepository.update(post);
+  }
 
-    @Override
-    @Transactional
-    public void removePost(int id) {
-        postRepository.deleteById(id);
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  @Transactional
+  public void removePost(int id) {
+    postRepository.deleteById(id);
+  }
 }
